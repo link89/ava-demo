4a513213edf58f8b7cd5fb23b233eb6321c9743b
Henry Xu <henry.xu@ringcentral.com>
2020-05-27 03:17:03 +0800

    feat(*): connect puppeteer via selenium

--- test.ts
+++ test.ts
@@ -1,17 +1,23 @@
 import test from 'ava';
 import { BrowserObject, remote } from "webdriverio";
+import * as puppeteer from 'puppeteer-core';
 
 
-const fn = () => 'foo';
-
-test('fn() returns foo', async (t) => {
+test('webdriver io cdp demo', async (t) => {
 	const driver: BrowserObject = await remote({
 		hostname: 'localhost',
 		port: 4444,
 		path: '/wd/hub',
 		capabilities: {
 			browserName: 'chrome',
-  },});
+		},
+	});
+	const debugAddress = driver.capabilities['goog:chromeOptions']?.debuggerAddress;
+	const puppeteerDriver = await puppeteer.connect({browserURL: `http://${debugAddress}`});
 
-	t.is(fn(), 'foo');
+	await driver.url('https://cn.bing.com')
+	const pages = await puppeteerDriver.pages();
+	console.log(`get title via puppeteer: ${await pages[0].title()}`);
+
+	t.pass();
 });
