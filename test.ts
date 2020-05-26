d49f342082e0dcd6f303fbe4e729a1f5e8d28c60
Henry Xu <henry.xu@ringcentral.com>
2020-05-27 03:26:06 +0800

    feat(*): collect coverage via puppeteer

--- test.ts
+++ test.ts
@@ -14,10 +14,14 @@ test('webdriver io cdp demo', async (t) => {
 	});
 	const debugAddress = driver.capabilities['goog:chromeOptions']?.debuggerAddress;
 	const puppeteerDriver = await puppeteer.connect({browserURL: `http://${debugAddress}`});
-
-	await driver.url('https://cn.bing.com')
 	const pages = await puppeteerDriver.pages();
-	console.log(`get title via puppeteer: ${await pages[0].title()}`);
+	await pages[0].coverage.startJSCoverage();
+
+	await driver.url('https://baidu.com');
+  console.log(`get title via puppeteer: ${await pages[0].title()}`);
+
+	const coverage = await pages[0].coverage.stopJSCoverage();
+	console.log(coverage);
 
 	t.pass();
 });
