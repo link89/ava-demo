e3f768157f819be00eb4da5175e87f975c2621a8
Henry Xu <henry.xu@ringcentral.com>
2020-05-27 02:09:13 +0800

    chore(*): open browser

--- test.ts
+++ test.ts
@@ -1,7 +1,17 @@
 import test from 'ava';
+import { BrowserObject, remote } from "webdriverio";
+
 
 const fn = () => 'foo';
 
-test('fn() returns foo', t => {
+test('fn() returns foo', async (t) => {
+	const driver: BrowserObject = await remote({
+		hostname: 'localhost',
+		port: 4444,
+		path: '/wd/hub',
+		capabilities: {
+			browserName: 'chrome',
+  },});
+
 	t.is(fn(), 'foo');
 });
