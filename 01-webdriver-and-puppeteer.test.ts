dc637ca0c5a4434e8a296dcf39204b3f2f660932
Henry Xu <henry.xu@ringcentral.com>
2020-05-27 09:24:40 +0800

    doc(*): add readme and comments

--- 01-webdriver-and-puppeteer.test.ts
+++ 01-webdriver-and-puppeteer.test.ts
@@ -4,6 +4,7 @@ import * as puppeteer from 'puppeteer-core';
 
 
 test('webdriver io cdp demo', async (t) => {
+	// start webdriver
 	const driver: BrowserObject = await remote({
 		hostname: 'localhost',
 		port: 4444,
@@ -12,17 +13,24 @@ test('webdriver io cdp demo', async (t) => {
 			browserName: 'chrome',
 		},
 	});
+
+	// connect with puppeteer
 	const debugAddress = driver.capabilities['goog:chromeOptions']?.debuggerAddress;
 	const puppeteerDriver = await puppeteer.connect({browserURL: `http://${debugAddress}`});
+
+	// puppeteer: start to collect js coverage
 	const pages = await puppeteerDriver.pages();
 	await pages[0].coverage.startJSCoverage();
 
+	// do something with webdriver.io or puppeteer
 	await driver.url('https://baidu.com');
   console.log(`get title via puppeteer: ${await pages[0].title()}`);
 
+	// puppeteer: stop coverage collection
 	const coverage = await pages[0].coverage.stopJSCoverage();
 	console.log(coverage);
 
+	// clean up
   await driver.deleteSession();
 	t.pass();
 });
