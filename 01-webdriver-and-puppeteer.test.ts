2b4f94a0744994745d2e040af1a0362aad3f0698
Henry Xu <henry.xu@ringcentral.com>
2020-05-27 10:21:37 +0800

    chore(*): wip: conver to istanbul

--- 01-webdriver-and-puppeteer.test.ts
+++ 01-webdriver-and-puppeteer.test.ts
@@ -1,6 +1,8 @@
 import test from 'ava';
 import { BrowserObject, remote } from "webdriverio";
 import * as puppeteer from 'puppeteer-core';
+import * as fs from 'fs';
+import * as bluebird from "bluebird";
 
 
 test('webdriver io cdp demo', async (t) => {
@@ -20,15 +22,18 @@ test('webdriver io cdp demo', async (t) => {
 
 	// puppeteer: start to collect js coverage
 	const pages = await puppeteerDriver.pages();
-	await pages[0].coverage.startJSCoverage();
+	await pages[0].coverage.startJSCoverage({ resetOnNavigation: false });
 
 	// do something with webdriver.io or puppeteer
-	await driver.url('https://baidu.com');
+	await driver.url('https://jupiter.fiji.gliprc.com');
+	await bluebird.delay(5e3);
 	console.log(`get title via puppeteer: ${await pages[0].title()}`);
 
 	// puppeteer: stop coverage collection
+	// TODO: handle remap
 	const coverage = await pages[0].coverage.stopJSCoverage();
-	console.log(coverage);
+	fs.writeFileSync('v8-coverage.json', JSON.stringify(coverage, null, 2));
+
 
 	// clean up
 	await driver.deleteSession();
