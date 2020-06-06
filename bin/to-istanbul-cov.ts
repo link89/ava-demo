165a86eb0d40173aee36e5212ec6e0081b41bcd4
Henry Xu <henry.xu@ringcentral.com>
2020-06-07 01:09:07 +0800

    feat: convert to istanbul

--- bin/to-istanbul-cov.ts
+++ bin/to-istanbul-cov.ts
@@ -5,8 +5,9 @@ import * as bluebird from "bluebird";
 import * as https from 'https';
 import axios from 'axios';
 
-const v8ToIstanbul = require('v8-to-istanbul');  // FIXME
+const v8ToIstanbul = require('v8-to-istanbul');
 const createSourceMapStore = require('istanbul-lib-source-maps').createSourceMapStore;
+const libCoverage = require('istanbul-lib-coverage');
 
 interface V8Coverage {
   url: string;
@@ -23,36 +24,37 @@ async function toIstanbulCov(cachePath: string, outputPath: string, v8CovFile: s
   if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath, { recursive: true });
 
   // download .js & .js.map
-  const sourcesPath = path.join(cachePath, 'sources');  // folder to store .js & .js.map files
+  const mapStore = createSourceMapStore();
+  const sourcesPath = path.join(cachePath, 'sources');
   fs.mkdirSync(sourcesPath, { recursive: true });
   const covWithSources = await bluebird.map(
     v8Covs.filter(script => script.url.startsWith('http') && script.url.endsWith('.js')), async (script) => {
       // download source file
       const sourceFilePath = path.join(sourcesPath, pathFromUrl(script.url));
-      let sourceMapFilePath: string;
+      const sourceMapFilePath = sourceFilePath + '.map';
       if (!fs.existsSync(sourceFilePath)) {
         fs.mkdirSync(path.dirname(sourceFilePath), { recursive: true });
         let res = await axios.get(script.url, { httpsAgent: new https.Agent({ rejectUnauthorized: false }) });
-        sourceMapFilePath = sourceFilePath + '.map';
         fs.writeFileSync(sourceFilePath, res.data);
         res = await axios.get(script.url + '.map', { transformResponse: [], httpsAgent: new https.Agent({ rejectUnauthorized: false }) });
         fs.writeFileSync(sourceMapFilePath, res.data);
       }
+      mapStore.registerURL(sourceFilePath, path.basename(sourceMapFilePath));
       return { script, sourceFilePath, sourceMapFilePath };
     }, { concurrency: 4, });
 
   // convert v8 coverage to istanbul
   if (!fs.existsSync(outputPath)) fs.mkdirSync(outputPath, { recursive: true });
-  const mapStore = createSourceMapStore();
-
-  await bluebird.map(covWithSources, async (obj) => {
+  await bluebird.map(covWithSources, async (obj, i) => {
     const converter = v8ToIstanbul(obj.sourceFilePath);
     await converter.load();
     converter.applyCoverage(obj.script.functions);
     const cov = converter.toIstanbul();
+    const covMap = libCoverage.createCoverageMap(cov);
+    const transformed = await mapStore.transformCoverage(covMap);
+    const istanbulFilePath = path.join(outputPath, `${i}.json`);
+    fs.writeFileSync(istanbulFilePath, JSON.stringify(transformed, null, 2));
   }, { concurrency: 4, });
-
-  // FIXME: v8-to-istanbul: source-mappings from one to many files not yet supported
 }
 
 function usage(message: string) {
